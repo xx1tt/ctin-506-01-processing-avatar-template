c19a6d1b479677b15bd04081c147b8775ce9d969
mastroni-edu <111853548+mastroni-edu@users.noreply.github.com>
2022-08-23 01:12:23 -0700

    Added fake avatar file

---
+++ avatar.java
@@ -0,0 +1,12 @@
+// edit this to be 512x512 pixels
+size(200, 100);
+
+// edit this if you want a different background color
+background(153);
+
+// add/replace any line of code below this to create your very own avatar!
+// hint: use shape primitives
+//        -triangle
+//        -rect
+//        -circle
+line(0, 0, width, height);
